17a11cda8a5cceb42d7f9e820748180aca672bd1
Madi <Madi.Abay@kcell.kz>
2024-04-03 06:18:52 +0500

    initial commit

---
+++ code/src/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    #api
+    path('api/', include('src.api_urls'))
+]
